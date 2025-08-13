610822e30ac080c9b4436ee0dd7b1fc6450f296a
ChrisKent <chrisgkent@gmail.com>
2025-08-13 17:13:08 +0100

    Added tests for primer lookup

---
+++ tests/test_main.py
@@ -0,0 +1,204 @@
+import pathlib
+import unittest
+from primalbedtools.scheme import Scheme
+from primalbedtools.bedfiles import merge_primers
+from primalbedtools.amplicons import create_amplicons
+
+from aligntrim.main import (
+    create_primer_lookup,
+    find_primer_with_lookup,
+    generate_amplicons,
+)
+
+BED_PATH_V5_3_2 = pathlib.Path(__file__).parent / "test_data/primer.bed"
+BED_PATH_V1_0_0 = pathlib.Path(__file__).parent / "test_data/v1.0.0.primer.bed"
+
+
+class TestCreatePrimerLookup(unittest.TestCase):
+    """
+    Tests for the create_primer_lookup function
+    """
+
+    def setUp(self):
+        self.scheme = Scheme.from_file(str(BED_PATH_V5_3_2.absolute()))
+        self.pools = {bl.pool for bl in self.scheme.bedlines}
+        self.scheme.bedlines = merge_primers(self.scheme.bedlines)
+        self.amplicons = create_amplicons(self.scheme.bedlines)
+
+        # Create ref len
+        self.ref_len = [("MN908947.3", 29903)]
+        return super().setUp()
+
+    def test_create_primer_lookup(self):
+        """
+        Test that the primer lookup is created correctly
+        """
+        # Create the primer lookup with 35 padding
+        padding = 0
+        primer_lookup = create_primer_lookup(
+            ref_len_tuple=self.ref_len,
+            amplicons=self.amplicons,
+            pools=self.pools,
+            padding=padding,
+        )
+        # Check that the primer lookup is a dictionary
+        self.assertIsInstance(primer_lookup, dict)
+        # Check that the primer lookup contains the expected keys
+        self.assertEqual(
+            set(x[0] for x in self.ref_len),
+            set(primer_lookup.keys()),
+        )
+
+        # Check the size of the primer lookup
+        self.assertEqual(
+            primer_lookup["MN908947.3"].shape,
+            (max(self.pools), 29903 + 1),  # +1 for half open interval
+        )
+
+        # Check each amplicon is present in the lookup and in correct pool
+        for amplicon in self.amplicons:
+            # Check amplicon spans correct region, and correct pool
+            self.assertEqual(
+                set(
+                    primer_lookup[amplicon.chrom][
+                        amplicon.ipool, amplicon.amplicon_start : amplicon.amplicon_end
+                    ]
+                ),
+                set([amplicon]),
+            )
+            # Check padding is aligned correctly
+            self.assertIsNone(
+                primer_lookup[amplicon.chrom][
+                    amplicon.ipool, amplicon.amplicon_start - padding - 1
+                ]
+            )
+            self.assertIsNone(
+                primer_lookup[amplicon.chrom][
+                    amplicon.ipool, amplicon.amplicon_end + padding  # -1 +1
+                ]
+            )
+
+    def test_create_primer_lookup_padding(self):
+        """
+        Test that the primer lookup is created correctly
+        """
+        # Create the primer lookup with 35 padding
+        padding = 10
+        primer_lookup = create_primer_lookup(
+            ref_len_tuple=self.ref_len,
+            amplicons=self.amplicons,
+            pools=self.pools,
+            padding=padding,
+        )
+        # Check that the primer lookup is a dictionary
+        self.assertIsInstance(primer_lookup, dict)
+        # Check that the primer lookup contains the expected keys
+        self.assertEqual(
+            set(x[0] for x in self.ref_len),
+            set(primer_lookup.keys()),
+        )
+
+        # Check the size of the primer lookup
+        self.assertEqual(
+            primer_lookup["MN908947.3"].shape,
+            (max(self.pools), 29903 + 1),  # +1 for half open interval
+        )
+
+        # Check each amplicon is present in the lookup and in correct pool
+        for amplicon in self.amplicons:
+            # Check amplicon spans correct region, and correct pool
+            self.assertEqual(
+                set(
+                    primer_lookup[amplicon.chrom][
+                        amplicon.ipool, amplicon.amplicon_start : amplicon.amplicon_end
+                    ]
+                ),
+                set([amplicon]),
+            )
+            # Check padding is aligned correctly
+            self.assertIsNone(
+                primer_lookup[amplicon.chrom][
+                    amplicon.ipool, amplicon.amplicon_start - padding - 1
+                ]
+            )
+            self.assertIsNone(
+                primer_lookup[amplicon.chrom][
+                    amplicon.ipool, amplicon.amplicon_end + padding  # -1 +1
+                ]
+            )
+
+
+class TestFindPrimerWithLookup(unittest.TestCase):
+    def setUp(self):
+        self.scheme = Scheme.from_file(str(BED_PATH_V5_3_2.absolute()))
+        self.pools = {bl.pool for bl in self.scheme.bedlines}
+        self.scheme.bedlines = merge_primers(self.scheme.bedlines)
+        self.amplicons = create_amplicons(self.scheme.bedlines)
+
+        # Create ref len and lookup
+        self.ref_len = [("MN908947.3", 29903)]
+        self.primer_lookup = create_primer_lookup(
+            ref_len_tuple=self.ref_len,
+            amplicons=self.amplicons,
+            pools=self.pools,
+            padding=0,
+        )
+
+        return super().setUp()
+
+    def test_find_primer(self):
+        # For each position in each amplicons ensure the correct primer is found
+        for amplicon in self.amplicons:
+            for fpos in range(amplicon.amplicon_start, amplicon.amplicon_start + 50):
+                # Find left
+                lp = find_primer_with_lookup(
+                    self.primer_lookup, fpos, "+", amplicon.chrom
+                )
+                self.assertEqual(lp, amplicon.left[0])
+            for rpos in range(amplicon.amplicon_end - 50, amplicon.amplicon_end):
+                # Find Right
+                rp = find_primer_with_lookup(
+                    self.primer_lookup, rpos, "-", amplicon.chrom
+                )
+                self.assertEqual(rp, amplicon.right[0])
+
+
+class TestGenerateAmplicon(unittest.TestCase):
+    def setUp(self):
+        self.scheme = Scheme.from_file(str(BED_PATH_V5_3_2.absolute()))
+        self.scheme.bedlines = merge_primers(self.scheme.bedlines)
+        return super().setUp()
+
+    def test_generate_amplicons(self):
+        amplicon_dict = generate_amplicons(self.scheme.bedlines)
+        pbt_amps = create_amplicons(self.scheme.bedlines)
+
+        # Check the amplicon dict agrees with pbt
+        for chrom, amp_dict in amplicon_dict.items():
+            chrom_pbt_amps = [amp for amp in pbt_amps if amp.chrom == chrom]
+
+            for amp_num, stats_dict in amp_dict.items():
+                # Get the corresponding amp
+                pbt_amp = [
+                    amp for amp in chrom_pbt_amps if amp.amplicon_number == amp_num
+                ][0]
+                self.assertEqual(stats_dict["p_start"], pbt_amp.amplicon_start)
+                self.assertEqual(
+                    stats_dict["start"], pbt_amp.coverage_start + 1
+                )  # idk why +1
+                self.assertEqual(stats_dict["p_end"], pbt_amp.amplicon_end)
+                self.assertEqual(
+                    stats_dict["end"], pbt_amp.coverage_end - 1
+                )  # idk why -1
+
+
+class TestTrim(unittest.TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def test_trim_basic(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
